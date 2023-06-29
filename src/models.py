4d0b85c27a96c89ae80a32fba3fca32fcc421c49
ejmockler <emockler@ucsc.edu>
2023-06-28 18:27:12 -0700

    switch to BernoulliNB for binary features

--- src/models.py
+++ src/models.py
@@ -3,7 +3,7 @@ from sklearn.ensemble import (
     RandomForestClassifier,
 )
 from sklearn.linear_model import LogisticRegression
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.naive_bayes import BernoulliNB
 from sklearn.svm import LinearSVC, SVC
 from xgboost import XGBClassifier
 from pytorch_tabnet.tab_model import TabNetClassifier
@@ -29,12 +29,12 @@ stack = {
         "tol": Real(1e-6, 1e1, prior="log-uniform"),
         "C": Real(1e-4, 1e1, prior="log-uniform"),
     },
-    MultinomialNB(): {"alpha": Real(1e-10, 1e1, prior="log-uniform")},
+    BernoulliNB(): {"alpha": Real(1e-10, 1e1, prior="log-uniform")},
     AdaBoostClassifier(): {
         "n_estimators": Integer(25, 75),
         "learning_rate": Real(1e-6, 1e1, prior="log-uniform"),
     },
-    XGBClassifier(): {  # single thread to avoid memory issues (segfaults) in multiprocessing
+    XGBClassifier(): {
         "learning_rate": Real(1e-6, 1e1, prior="log-uniform"),
         "n_estimators": Integer(10, 100),
     },
