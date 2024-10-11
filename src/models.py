ef4a85b09551128c6548a4f00672567f1474c19b
ejmockler <emockler@ucsc.edu>
2024-10-11 01:27:49 -0700

    explicit kwargs required in constructor

--- src/models.py
+++ src/models.py
@@ -14,12 +14,78 @@ from lightgbm import LGBMClassifier
 from skopt.space import Integer, Real
 
 class RadialBasisSVC(SVC):
-    def __init__(self, **kwargs):
-        super().__init__(kernel="rbf", **kwargs)
+    def __init__(
+        self,
+        C=1.0,
+        kernel="rbf",
+        degree=3,
+        gamma="scale",
+        coef0=0.0,
+        shrinking=True,
+        probability=False,
+        tol=1e-3,
+        cache_size=200,
+        class_weight=None,
+        verbose=False,
+        max_iter=-1,
+        decision_function_shape="ovr",
+        break_ties=False,
+        random_state=None
+    ):
+        super().__init__(
+            C=C,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state
+        )
 
 class LinearSVC(SVC):
-    def __init__(self, **kwargs):
-        super().__init__(kernel="linear", **kwargs)
+    def __init__(
+        self,
+        C=1.0,
+        kernel="linear",
+        degree=3,
+        gamma="scale",
+        coef0=0.0,
+        shrinking=True,
+        probability=False,
+        tol=1e-3,
+        cache_size=200,
+        class_weight=None,
+        verbose=False,
+        max_iter=-1,
+        decision_function_shape="ovr",
+        break_ties=False,
+        random_state=None
+    ):
+        super().__init__(
+            C=C,
+            kernel=kernel,
+            degree=degree,
+            gamma=gamma,
+            coef0=coef0,
+            shrinking=shrinking,
+            probability=probability,
+            tol=tol,
+            cache_size=cache_size,
+            class_weight=class_weight,
+            verbose=verbose,
+            max_iter=max_iter,
+            decision_function_shape=decision_function_shape,
+            break_ties=break_ties,
+            random_state=random_state
+        )
 
 class SparseBayesianLogisticRegression(BaseEstimator, ClassifierMixin):
     def __init__(self, num_iterations=1000, lr=0.01, verbose=False):
